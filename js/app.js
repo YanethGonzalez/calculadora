fe3791b50a73c85e8c61bdaf363c046d83c35151
Yaneth Gonzalez <yanethgonzalez680@gmail.com>
2025-03-19 10:25:54 -0500

    se inicializa proyecto

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let texto = document.getElementById("texto");
+let operacion = document.getElementById("operacion");
+let boton = document.getElementById("calcula");
+
+boton.addEventListener("click", calcula);
+
+function calcula(){
+}
