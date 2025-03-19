b8a2e0898c8bd8fa6e283b7f240ec7019c396432
Yaneth Gonzalez <yanethgonzalez680@gmail.com>
2025-03-19 18:06:33 -0500

    se crea funcion para realizar calculos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
-let texto = document.getElementById("texto");
+let primerNumero = document.getElementById("primerNumero");
+let segundoNumero = document.getElementById("segundoNumero");
+let resultado = document.getElementById("resultado");
 let operacion = document.getElementById("operacion");
 let boton = document.getElementById("calcula");
+
 
 boton.addEventListener("click", calcula);
 
 function calcula(){
+  primerNumero = Number(primerNumero.value);
+  segundoNumero = Number(segundoNumero.value);
+
+if ( operacion.value == 1 ) {
+  console.log(primerNumero + segundoNumero)
 }
+
+else if ( operacion.value == 2 ) {
+  console.log(primerNumero - segundoNumero)
+}
+  
+else if (operacion.value == 3) {
+  console.log(primerNumero * segundoNumero)
+}
+
+else if (operacion.value == 4) {
+  console.log(primerNumero / segundoNumero)
+}
+
+else {
+  
+  console.log("selecciona la operacion a realizar")
+}
+}
+
